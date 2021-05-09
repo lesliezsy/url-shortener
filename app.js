8e14d5fc4a87e9fddd2f5fd171487ca9f55cd6c5
Leslie <lesliezsy12@gmail.com>
2021-05-09 22:00:43 +0800

    project init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const { urlencoded } = require("body-parser")
+
+const app = express()
+
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
+
+app.set('view engine', 'hbs')
+app.use(urlencoded({ extended: true })) // body-parser
+app.use(express.static('public')) // 如果會用到靜態資料夾 public
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
